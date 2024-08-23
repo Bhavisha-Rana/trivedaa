aba9c7e4956dfe20d8b1f4ca48deaaf45b5c5a83
Bhavisha-Rana <bhavishaa.easternts@gmail.com>
2024-08-23 18:15:48 +0530

    setup theme

---
+++ wp-content/themes/trivedaa/front-page.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The front-page.php corresponds to the "static front page"
+ * when setting static front page in Settings > Reading 
+ */
+?>
+
+<?php get_header(); ?>
+
+<header class="header">
+        <div class="video-fullscreen-wrap">
+            <div class="video-fullscreen-video">
+                <video playsinline="" autoplay="" loop="" muted="">
+                    <source src="https://duruthemes.com/demo/html/bauen/img/video.mp4" type="video/mp4" autoplay="" loop="" muted="">
+                    <source src="https://duruthemes.com/demo/html/bauen/img/video.webm" type="video/webm" autoplay="" loop="" muted="">
+                </video>
+            </div>
+            <div class="v-middle mt-30">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-7 text-left">
+                            <div class="video-fullscreen-inner">
+                                <h1>Innovate Design in Toronto</h1>
+                                <p>Architecture viverra tellus nec massa dictum the euismoe.<br>Curabitur viverra the posuere aukue velit.</p>
+                                <div class="butn-light mt-30 mb-30"><a href="https://1.envato.market/mDnXD" target="_blank"><span>Buy Now</span></a></div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+<?php get_footer(); ?>
